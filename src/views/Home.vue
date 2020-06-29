2839260a56886e2bdbed32c77794ed66b16972ac
Heiko Beineke <heiko.beineke@gmail.com>
2020-06-29 22:43:03 +0200

    Introduce layouts
    https://markus.oberlehner.net/blog/dynamic-vue-layout-components/

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="home">
-    <a href="#" @click="currentUserLogout"> logout </a>
-    <router-link to="/login">Login</router-link>
-    <router-link to="/register">Register</router-link>
-  </div>
+  <LayoutDefaultDynamic>
+    <div class="home">
+      <a href="#" @click="currentUserLogout"> logout </a>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/register">Register</router-link>
+    </div>
+  </LayoutDefaultDynamic>
 </template>
 
 <script>
-// @ is an alias to /src
 import { mapActions } from 'vuex';
+import LayoutDefaultDynamic from '@/layouts/LayoutDefaultDynamic';
 
 export default {
   name: 'Home',
+  components: {
+    LayoutDefaultDynamic,
+  },
   methods: {
     ...mapActions('users', ['currentUserLogout']),
   },
